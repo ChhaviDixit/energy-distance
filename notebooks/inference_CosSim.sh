2e406fd8e9125f53306c9be57b1a206392aa06ed
ChhaviDixit <48754594+ChhaviDixit@users.noreply.github.com>
2025-05-08 22:36:30 -0400

    Test for trained model

--- notebooks/inference_CosSim.sh
+++ notebooks/inference_CosSim.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=mteb-eval_hotpotqa          # The job name.
 #SBATCH -c 2                    # The number of cpu cores to use.
 #SBATCH --time=48:00:00            # The time the job will take to run
-#SBATCH --mem-per-cpu=1gb	# The memory the job will use per cpu core.
+#SBATCH --mem-per-cpu=16gb	# The memory the job will use per cpu core.
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # MODIFY THE FOLLOWING FOR DIFFERENT CONFIGURATIONS
@@ -12,20 +12,66 @@
 #SBATCH --gres=gpu:1            # Number of GPUs per node
 #SBATCH --ntasks-per-node=1     # Number of processes per node
 
-module load anaconda
-module load cuda92/toolkit
+# module purge
+
+# module load anaconda
+# module load cuda/12.3
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# source ~/.bashrc
+# # conda activate testenv           # Activate the virtual environment
+# source /insomnia001/depts/edu/COMSE6998/cd3496/miniconda3/etc/profile.d/conda.sh
+# conda activate myenv39           # Activate the virtual environment
+# nvidia-smi
+# pip list
+# # Set PYTHONPATH to prioritize the virtual environment
+# export PYTHONPATH=$CONDA_PREFIX/lib/python3.9/site-packages:$PYTHONPATH
+# # echo $PATH
+# # echo $PYTHONPATH
+# export PYTHONPATH=/insomnia001/home/cd3496/mteb:$PYTHONPATH
+# pip list
+# #export CUDA_VISIBLE_DEVICES=1
+
+# srun python /insomnia001/home/cd3496/energy-distance/notebooks/eval_dataset.py
+# #srun python /moto/home/ggn2104/cos_sim/eval_dataset_subset_length.py 
+
+# # End of script
+
+module purge
+
+# RPI Cluster
+module load gcc/14.1.0
+module load cuda
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 source ~/.bashrc
-conda activate testenv           # Activate the virtual environment
+# source ~/barn/miniconda3x86/etc/profile.d/conda.sh  # Adjust path if needed (RPI Cluster only)
+source /insomnia001/depts/edu/COMSE6998/cd3496/miniconda3/etc/profile.d/conda.sh
+
+
+conda activate myenv39           # Activate the virtual environment for ED model training
+#conda activate testenv          # Activate the virtual environment for CosSim model training
+
 nvidia-smi
 
 # Set PYTHONPATH to prioritize the virtual environment
 export PYTHONPATH=$CONDA_PREFIX/lib/python3.9/site-packages:$PYTHONPATH
+export PYTHONPATH=/insomnia001/home/cd3496/mteb:$PYTHONPATH
 
-#export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1  # Only use GPU 0
 
-srun python /moto/home/ggn2104/cos_sim/eval_dataset.py
-#srun python /moto/home/ggn2104/cos_sim/eval_dataset_subset_length.py 
+# Start nvidia-smi in the background to monitor GPU utilization every 10 seconds
+#while true; do
+ #   nvidia-smi >> gpu_utilization.log
+ #   sleep 60
+#done &
 
+
+#srun python /gpfs/u/home/MSSV/MSSVntsn/barn/beir/examples/retrieval/training/train_sbert4.py
+#srun python /gpfs/u/home/MSSV/MSSVntsn/barn/beir/examples/retrieval/training/train_sbert_latest.py
+# srun python /gpfs/u/home/MSSV/MSSVntsn/barn/beir/examples/retrieval/training/train_sbert_latest_2.py
+# srun python /insomnia001/home/cd3496/energy-distance/notebooks/eval_dataset.py
+srun python /insomnia001/home/cd3496/energy-distance/notebooks/eval_dataset_subset_length.py
+nvidia-smi
 # End of script
+
